c40558d5c2943e8eee06f38810479ced91300b6f
Lance Harper <lancetharper@gmail.com>
2017-02-13 15:02:08 -0800

    Add data.json back from gitignore, add missing row markup

--- css-report/src/components/Page.vue
+++ css-report/src/components/Page.vue
@@ -7,20 +7,22 @@
       <button type="button" class="btn btn-primary btn-sm" @click="collapsed = !collapsed">{{collapsed ? 'View Page Details': 'Hide Page Details'}}</button>
     </div>
     <div class="card-block" v-if="!collapsed">
-      <stat-header :count="pageData.uniques.color.length" :name="''" :plural="'Colors'" :singular="'Color'"></stat-header>
-      <detail-colors v-for="color in pageData.uniques.color" :color="color"></detail-colors>
+      <div class="row row-flex">
+        <stat-header :count="pageData.uniques.color.length" :name="''" :plural="'Colors'" :singular="'Color'"></stat-header>
+        <detail-colors v-for="color in pageData.uniques.color" :color="color"></detail-colors>
 
-      <stat-header :count="pageData.uniques.backgroundColor.length" :name="'Background'" :plural="'Colors'" :singular="'Color'"></stat-header>
-      <detail-bg-colors v-for="color in pageData.uniques.backgroundColor" :color="color"></detail-bg-colors>
+        <stat-header :count="pageData.uniques.backgroundColor.length" :name="'Background'" :plural="'Colors'" :singular="'Color'"></stat-header>
+        <detail-bg-colors v-for="color in pageData.uniques.backgroundColor" :color="color"></detail-bg-colors>
 
-      <stat-header :count="pageData.uniques.fontSize.length" :name="'Font'" :plural="'Sizes'" :singular="'Size'"></stat-header>
-      <detail-text v-for="value in pageData.uniques.fontSize" :value="value"></detail-text>
+        <stat-header :count="pageData.uniques.fontSize.length" :name="'Font'" :plural="'Sizes'" :singular="'Size'"></stat-header>
+        <detail-text v-for="value in pageData.uniques.fontSize" :value="value"></detail-text>
 
-      <stat-header :count="pageData.uniques.fontFamily.length" :name="'Font'" :plural="'Families'" :singular="'Family'"></stat-header>
-      <detail-text v-for="value in pageData.uniques.fontFamily" :value="value"></detail-text>
+        <stat-header :count="pageData.uniques.fontFamily.length" :name="'Font'" :plural="'Families'" :singular="'Family'"></stat-header>
+        <detail-text v-for="value in pageData.uniques.fontFamily" :value="value"></detail-text>
 
-      <stat-header :count="pageData.uniques.mediaQueries.length" :name="'Media'" :plural="'Queries'" :singular="'Query'"></stat-header>
-      <detail-text v-for="value in pageData.uniques.mediaQueries" :value="value"></detail-text>
+        <stat-header :count="pageData.uniques.mediaQueries.length" :name="'Media'" :plural="'Queries'" :singular="'Query'"></stat-header>
+        <detail-text v-for="value in pageData.uniques.mediaQueries" :value="value"></detail-text>
+      </div>
 
       <sheet v-for="data in pageData.styleData" :data="data"></sheet>
     </div>
--- css-report/src/components/Sheet.vue
+++ css-report/src/components/Sheet.vue
@@ -6,30 +6,32 @@
       <button type="button" class="btn btn-primary btn-sm" @click="collapsed = !collapsed">{{collapsed ? 'View File Details': 'Hide File Details'}}</button>
     </div>
     <div class="card-block" v-if="!collapsed">
-      <stat-header :count="data.trueUniques.color.length" :name="'Unique'" :plural="'Colors'" :singular="'Color'"></stat-header>
-      <detail-colors v-for="color in data.trueUniques.color" :color="color"></detail-colors>
-      <!-- <stat-header :count="data.uniques.data.color.length" :name="''" :plural="'Colors'" :singular="'Color'"></stat-header>
-      <detail-colors v-for="color in data.uniques.data.color" :color="color"></detail-colors> -->
+      <div class="row row-flex">
+        <stat-header :count="data.trueUniques.color.length" :name="'Unique'" :plural="'Colors'" :singular="'Color'"></stat-header>
+        <detail-colors v-for="color in data.trueUniques.color" :color="color"></detail-colors>
+        <!-- <stat-header :count="data.uniques.data.color.length" :name="''" :plural="'Colors'" :singular="'Color'"></stat-header>
+        <detail-colors v-for="color in data.uniques.data.color" :color="color"></detail-colors> -->
 
-      <stat-header :count="data.trueUniques.backgroundColor.length" :name="'Unique Background'" :plural="'Colors'" :singular="'Color'"></stat-header>
-      <detail-bg-colors v-for="color in data.trueUniques.backgroundColor" :color="color"></detail-bg-colors>
-      <!-- <stat-header :count="data.uniques.data.backgroundColor.length" :name="'Background'" :plural="'Colors'" :singular="'Color'"></stat-header>
-      <detail-bg-colors v-for="color in data.uniques.data.backgroundColor" :color="color"></detail-bg-colors> -->
+        <stat-header :count="data.trueUniques.backgroundColor.length" :name="'Unique Background'" :plural="'Colors'" :singular="'Color'"></stat-header>
+        <detail-bg-colors v-for="color in data.trueUniques.backgroundColor" :color="color"></detail-bg-colors>
+        <!-- <stat-header :count="data.uniques.data.backgroundColor.length" :name="'Background'" :plural="'Colors'" :singular="'Color'"></stat-header>
+        <detail-bg-colors v-for="color in data.uniques.data.backgroundColor" :color="color"></detail-bg-colors> -->
 
-      <stat-header :count="data.trueUniques.fontSize.length" :name="'Unique Font'" :plural="'Sizes'" :singular="'Size'"></stat-header>
-      <detail-text v-for="value in data.trueUniques.fontSize" :value="value"></detail-text>
-      <!-- <stat-header :count="data.uniques.data.fontSize.length" :name="'Font'" :plural="'Sizes'" :singular="'Size'"></stat-header>
-      <detail-text v-for="value in data.uniques.data.fontSize" :value="value"></detail-text> -->
+        <stat-header :count="data.trueUniques.fontSize.length" :name="'Unique Font'" :plural="'Sizes'" :singular="'Size'"></stat-header>
+        <detail-text v-for="value in data.trueUniques.fontSize" :value="value"></detail-text>
+        <!-- <stat-header :count="data.uniques.data.fontSize.length" :name="'Font'" :plural="'Sizes'" :singular="'Size'"></stat-header>
+        <detail-text v-for="value in data.uniques.data.fontSize" :value="value"></detail-text> -->
 
-      <stat-header :count="data.trueUniques.fontFamily.length" :name="'Unique Font'" :plural="'Families'" :singular="'Family'"></stat-header>
-      <detail-text v-for="value in data.trueUniques.fontFamily" :value="value"></detail-text>
-      <!-- <stat-header :count="data.uniques.data.fontFamily.length" :name="'Font'" :plural="'Families'" :singular="'Family'"></stat-header>
-      <detail-text v-for="value in data.uniques.data.fontFamily" :value="value"></detail-text> -->
+        <stat-header :count="data.trueUniques.fontFamily.length" :name="'Unique Font'" :plural="'Families'" :singular="'Family'"></stat-header>
+        <detail-text v-for="value in data.trueUniques.fontFamily" :value="value"></detail-text>
+        <!-- <stat-header :count="data.uniques.data.fontFamily.length" :name="'Font'" :plural="'Families'" :singular="'Family'"></stat-header>
+        <detail-text v-for="value in data.uniques.data.fontFamily" :value="value"></detail-text> -->
 
-      <stat-header :count="data.trueUniques.mediaQueries.length" :name="'Unique Media'" :plural="'Queries'" :singular="'Query'"></stat-header>
-      <detail-text v-for="value in data.trueUniques.mediaQueries" :value="value"></detail-text>
-      <!-- <stat-header :count="data.uniques.data.mediaQueries.length" :name="'Media'" :plural="'Queries'" :singular="'Query'"></stat-header>
-      <detail-text v-for="value in data.uniques.data.mediaQueries" :value="value"></detail-text> -->
+        <stat-header :count="data.trueUniques.mediaQueries.length" :name="'Unique Media'" :plural="'Queries'" :singular="'Query'"></stat-header>
+        <detail-text v-for="value in data.trueUniques.mediaQueries" :value="value"></detail-text>
+        <!-- <stat-header :count="data.uniques.data.mediaQueries.length" :name="'Media'" :plural="'Queries'" :singular="'Query'"></stat-header>
+        <detail-text v-for="value in data.uniques.data.mediaQueries" :value="value"></detail-text> -->
+      </div>
     </div>
   </section>
 </template>
